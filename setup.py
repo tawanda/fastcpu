996336e319d4caa0aea86e75934f95a665c2bee7
tawanda <tminya@gmail.com>
2021-03-05 00:09:06 +0200

    v1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fastcpu",
-    version="0.0.6",
+    version="1.0.0",
     author="Tawanda Minya",
     author_email="tminya@gmail.com",
     description=" A queue service for quickly developing scripts that use all your CPUs efficiently",
